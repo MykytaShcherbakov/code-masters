c863275ffb537bd7c2ffa2042d49536a45197f22
Andrii Boiko <andyboy7777777@gmail.com>
2025-07-04 14:49:47 +0200

    #GP-6___Second try

--- GardenProducts/src/App.jsx
+++ GardenProducts/src/App.jsx
@@ -1,14 +1,8 @@
-import { Route, Routes } from "react-router";
-import Categories from "./pages/Categories/Categories";
-import Home from "./pages/Home/Home";
 import "./App.css";
 
 const App = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/categories" element={<Categories />}/>       
-      </Routes>
+      <></>
   );
 };
 
