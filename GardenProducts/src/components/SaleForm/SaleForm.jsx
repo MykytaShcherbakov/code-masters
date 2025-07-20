4aa54e1a9dcdef6f3bdcb4bd0e1fea4c6660aee3
Andrii Boiko <andyboy7777777@gmail.com>
2025-07-20 14:14:05 +0200

    Features/discount form (#29) #GP22
    * Development (#2)
    
    * Wrong kommit
    
    * Test from Ol 2
    
    ---------
    
    Co-authored-by: Oleg Pykhonin <stoppard23@gmail.com>
    Co-authored-by: Andrii Boiko <andyboy7777777@gmail.com>
    
    * Mykyta (#3)
    
    * Wrong kommit
    
    * Test from Ol 2
    
    ---------
    
    Co-authored-by: Oleg Pykhonin <stoppard23@gmail.com>
    Co-authored-by: Andrii Boiko <andyboy7777777@gmail.com>
    
    * Development (#4)
    
    * Wrong kommit
    
    * Test from Ol 2
    
    ---------
    
    Co-authored-by: Andrii Boiko <andyboy7777777@gmail.com>
    
    * Update main
    
    * Mykyta (#5)
    
    * Wrong kommit
    
    * Test from Ol 2
    
    * Added components
    
    ---------
    
    Co-authored-by: Oleg Pykhonin <stoppard23@gmail.com>
    Co-authored-by: Andrii Boiko <andyboy7777777@gmail.com>
    
    * Development (#7)
    
    * Wrong kommit
    
    * Test from Ol 2
    
    * Added components (#6)
    
    ---------
    
    Co-authored-by: Oleg Pykhonin <stoppard23@gmail.com>
    Co-authored-by: Andrii Boiko <andyboy7777777@gmail.com>
    
    * Added Pop-Up CategorieProducts Changes Categories SaleForm
    
    ---------
    
    Co-authored-by: MykytaShcherbakov <shcherbakov.mykyta.s@gmail.com>
    Co-authored-by: Oleg Pykhonin <stoppard23@gmail.com>

---
+++ GardenProducts/src/components/SaleForm/SaleForm.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./SaleForm.scss";
+import Input from "../UI/Input/Input";
+import Button from "../UI/Button/Button";
+
+const SaleForm = () => {
+  const [submitMessage, setSubmitMessage] = useState(""); // Для подтверждающего сообщения
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = () => {
+    setSubmitMessage("The discount has been successfully sent by email"); // Текст сообщения при удачной отправки формы
+    reset(); // Обнуление формы после подтверждения
+  };
+
+  // Определяем, есть ли какие-либо ошибки
+  const hasErrors = Object.keys(errors).length > 0;
+
+  return (
+    <section className="sale__form-section">
+      <h3>5% off on the first order</h3>
+      <div className="sale__form-container">
+        <div className="sale__form-image__container">
+          <img
+            src="../../../public/Pictures/Discount/discount-image.png"
+            alt="image"
+            className="sale__form-image"
+          />
+        </div>
+        <form className="sale__form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="sale__form-inputs">
+            <div className="input__container">
+              <Input
+                typeName="text"
+                placeholderName="Name"
+                formClass="sale__form-control"
+                registerData={register("name", {
+                  // Регистрируем инпут с именем
+                  required: "Name is required",
+                })}
+              />
+            </div>
+            <div className="input__container">
+              <Input
+                typeName="text"
+                placeholderName="Phone number"
+                formClass="sale__form-control"
+                registerData={register("phone", {
+                  // Регистрируем инпут с телефоном
+                  required: "Phone number is required",
+                  pattern: {
+                    value: /^\d+$/, // Проверяем телефон
+                    message: "Wrong input. Try again",
+                  },
+                })}
+              />
+            </div>
+            <div className="input__container">
+              <Input
+                typeName="email"
+                placeholderName="Email"
+                formClass="sale__form-control"
+                registerData={register("email", {
+                  // Регистрируем инпут с почтой
+                  required: "Email address is required",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Проверяем почту
+                    message: "Wrong input. Try again",
+                  },
+                })}
+              />
+
+              {/* Если есть ошибки, выводим сообщение */}
+              {hasErrors && (
+                <p className="error-message">Wrong input. Try again</p>
+              )}
+              {/* Сообщение при успешном заполнении формы */}
+              {!hasErrors && submitMessage && (
+                <p className="submit__message">{submitMessage}</p>
+              )}
+            </div>
+          </div>
+          <Button
+            type="submit"
+            btnColor={submitMessage ? "submitted" : "white"}
+            btnText={submitMessage ? "Request Submitted" : "Get a discount"}
+            btnSize={"L"}
+          />
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default SaleForm;
---
+++ GardenProducts/src/pages/CategorieProducts/CategorieProducts.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import { useLoaderData, useParams, Link } from "react-router-dom";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import "./CategorieProducts.scss";
+
+export default function CategorieProducts() {
+  const { categoryId } = useParams();
+  const { products, category } = useLoaderData();
+
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+  const [sortOrder, setSortOrder] = useState("default");
+  const [showDiscountedOnly, setShowDiscountedOnly] = useState(false);
+
+  const min = parseFloat(minPrice) || 0;
+  const max = parseFloat(maxPrice) || Infinity;
+
+  // 1. Фильтрация по категории
+  const categoryFilteredProducts = products.filter(
+    (product) => product.categoryId === +categoryId
+  );
+
+  // 2. Фильтрация по цене
+  const priceFilteredProducts = categoryFilteredProducts.filter((product) => {
+    const realPrice = product.discont_price ?? product.price;
+    return realPrice >= min && realPrice <= max;
+  });
+
+  // 3. Фильтрация по скидке
+  const discountedFilteredProducts = showDiscountedOnly
+    ? priceFilteredProducts.filter((product) => product.discont_price !== null)
+    : priceFilteredProducts;
+
+  // 4. Сортировка
+  let sortedProducts = [...discountedFilteredProducts];
+  if (sortOrder === "price-asc") {
+    sortedProducts.sort((a, b) => {
+      const priceA = a.discont_price ?? a.price;
+      const priceB = b.discont_price ?? b.price;
+      return priceA - priceB;
+    });
+  } else if (sortOrder === "price-desc") {
+    sortedProducts.sort((a, b) => {
+      const priceA = a.discont_price ?? a.price;
+      const priceB = b.discont_price ?? b.price;
+      return priceB - priceA;
+    });
+  }
+
+  return (
+    <div className="container">
+      <nav aria-label="Breadcrumb" className="breadcrumbs">
+        <ol className="breadcrumb-list">
+          <li className="breadcrumb-item">
+            <Link to="/" className="breadcrumb-text">
+              Main page
+            </Link>
+          </li>
+          <li className="breadcrumb-separator" aria-hidden="true">
+            <span className="breadcrumb-line" />
+          </li>
+          <li className="breadcrumb-item">
+            <span className="breadcrumb-text-2">
+              {category ? category.title : "Category Products"}
+            </span>
+          </li>
+        </ol>
+      </nav>
+
+      <h1 className="page-title">
+        {category ? category.title : "Category Products"}
+      </h1>
+
+      <div className="filters-panel">
+        <div className="filter-group">
+          <label htmlFor="price-from" className="filter-label">
+            Price
+          </label>
+          <input
+            type="number"
+            id="price-from"
+            placeholder="from"
+            className="filter-input"
+            value={minPrice}
+            onChange={(e) => setMinPrice(e.target.value)}
+          />
+          <input
+            type="number"
+            id="price-to"
+            placeholder="to"
+            className="filter-input"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+          />
+        </div>
+
+        <div className="checkbox-container">
+          <label className="checkbox-label" htmlFor="discounted-items">
+            Discounted items
+          </label>
+          <input
+            className="checkbox-input"
+            type="checkbox"
+            id="discounted-items"
+            checked={showDiscountedOnly}
+            onChange={(e) => setShowDiscountedOnly(e.target.checked)}
+          />
+        </div>
+
+        <div className="filter-group-sorted">
+          <label htmlFor="sort-by" className="filter-label">
+            Sorted
+          </label>
+          <select
+            id="sort-by"
+            className="filter-select"
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <option value="default">by default</option>
+            <option value="price-asc">Price: Low to High</option>
+            <option value="price-desc">Price: High to Low</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="product-grid">
+        {sortedProducts.length > 0 ? (
+          sortedProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        ) : (
+          <p className="no-products-on-sale">No products found</p>
+        )}
+      </div>
+    </div>
+  );
+}
