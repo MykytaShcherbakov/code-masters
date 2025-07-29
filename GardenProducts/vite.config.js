51c6f0061287fc6600209b1c92f40f381937b5a6
Mykyta <shcherbakov.mykyta.s@gmail.com>
2025-07-29 16:29:43 +0200

    test 11

--- GardenProducts/vite.config.js
+++ GardenProducts/vite.config.js
@@ -3,16 +3,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
+// Получаем PORT только если он есть, чтобы не падало на build
+const PORT = process.env.PORT ? Number(process.env.PORT) : 4173;
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
     },
-  },
-  preview: {
-    host: true,
-    port: Number(import.meta.env.PORT) || 4173,
-    allowedHosts: ['code-masters.onrender.com'],
-  },
+    // preview используется только при vite preview
+    ...(command === 'serve' || command === 'preview'
+      ? {
+          preview: {
+            host: true,
+            port: PORT,
+            allowedHosts: ['code-masters.onrender.com'],
+          },
+        }
+      : {}),
+  };
 });
